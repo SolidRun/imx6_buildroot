96ae67ceec73910d5bd8ca46a1e0d30d087808c9
Yazan Shhady <yazan.shhady@solid-run.com>
2022-06-30 16:27:02 +0300

    first commit - Add imx6 Solidrun buildroot support

---
+++ README.md
@@ -0,0 +1,52 @@
+# imx6_buildroot
+# SolidRun's i.MX6 based  build scripts
+
+## Introduction
+Main intention of this repository is to build a buildroot based build environment for i.MX6 based products.
+
+The build script provides ready to use images that can be deployed on a micro SD card.
+
+## Building Image
+
+The build script will check for required tools, clone and build images and place results in output/ directory.
+
+### Docker build (recommended)
+
+* Build the Docker image (<b>Just once</b>):
+
+```
+docker build --build-arg user=$(whoami) --build-arg userid=$(id -u) -t imx6 docker/
+```
+
+To check if the image exists in you machine, you can use the following command:
+
+```
+docker images | grep imx6
+```
+
+* Run the build script:
+```
+docker run --rm -it -v "$PWD":/imx6_build/imx6_buildroot -v /etc/gitconfig:/etc/gitconfig imx6:latest /bin/bash
+# Run the build script
+cd /imx6_build/imx6_buildroot && ./runme.sh
+```
+
+To Delete all containers:
+```
+docker rm -f $(docker ps -a -q)
+```
+
+### Native Build
+Simply:
+
+```
+./runme.sh
+```
+
+## Deploying
+In order to create a bootable SD card, plug in a micro SD into your machine and run the following, where sdX is the location of the SD card got probed into your machine -
+
+```
+umount /media/<relevant directory>
+sudo dd if=output/microsd-<hash>.img of=/dev/sdX
+```
---
+++ runme.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+set -e
+echo "test one start .....................!!"
+
+###################################################################################################################################
+#                                                       Global environment
+
+BUILDROOT_VERSION=2020.08.1
+
+export ARCH=arm
+export CROSS_COMPILE=$BASE_DIR/build/buildroot/output/host/bin/arm-linux-
+
+BASE_DIR=`pwd`
+mkdir -p $BASE_DIR/build
+
+SHALLOW=${SHALLOW:false}
+if [ "x$SHALLOW" == "xtrue" ]; then
+        SHALLOW_FLAG="--depth 500"
+fi
+
+REPO_PREFIX=`git log -1 --pretty=format:%h`
+
+# Get number of jobs in this machine to use with make command
+JOBS=$(getconf _NPROCESSORS_ONLN)
+
+###################################################################################################################################
+#                                                       INSTALL Packages
+
+PACKAGES_LIST="git make mtools bison coreutils u-boot-tools gcc-arm-linux-gnueabihf gcc-arm-linux-gnueabi python3 python3-pyelftools libssl-dev build-essential device-tree-compiler bc unzip tar util-linux binutils e2fsprogs gawk wget diffstat texinfo chrpath sed g++ bash patch cpio python2 rsync file python3-pip flex parted"
+
+set +e
+for i in $PACKAGES_LIST; do
+
+	#Check if package is installed
+	dpkg -s $i > /dev/null  2>&1
+
+	#If exit code is not 0 - package is not installed
+	if [ $? -ne 0 ]; then
+
+		echo "Package $i is not installed"
+		echo "If using apt package manager, you can install this tool using:"
+		echo "	sudo apt install $i"
+		echo "You can install all needed packages using:"
+		echo "	sudo apt install $PACKAGES_LIST"
+
+		exit -1
+	fi
+
+done
+
+###################################################################################################################################
+#                                                       INSTALL Buildroot
+
+if [[ ! -d $BASE_DIR/build/buildroot ]]; then
+	cd $BASE_DIR/build
+	git clone ${SHALLOW_FLAG} https://github.com/buildroot/buildroot -b $BUILDROOT_VERSION
+fi
+
+# Build buildroot
+echo "*** Building buildroot"
+cd $BASE_DIR/build/buildroot
+cp $BASE_DIR/configs/imx6_solidrun_defconfig configs/imx6_solidrun_defconfig
+make imx6_solidrun_defconfig
+make
+
+ls
+
+echo "Done....!!!!"
